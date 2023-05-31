9d6fe4a7e18ebcd709024498099b0d1e350cb85a
Jaskirat Singh <Jaskirat.Singh@microsoft.com>
2023-05-31 09:31:30 +0530

    Enchancements to Host Output

--- MassRegistrationOfSAPSystems/RegisterSIDs.ps1
+++ MassRegistrationOfSAPSystems/RegisterSIDs.ps1
@@ -2,7 +2,7 @@
 #Connect-AzAccount -SubscriptionId 'yyyy-yyyy-yyyy-yyyy'
 
 # Replace <filePath> to the input CSV file
-$file = Import-CSV "<filePath>"
+$file = Import-CSV "C:\Users\jasksingh\Downloads\RegistrationData.csv"
 
 $MonitoringIntervalInSeconds = 30
 
@@ -33,15 +33,18 @@ foreach($line in $file)
     Start-Job -ScriptBlock $ScriptBlockCopy -ArgumentList $ResourceGroup, $SID, $Location, $Environment, $Product, $CentralServerVmId, $ManagedRgName, $MsiID -Name $random
 
     # Storing Job Definitions
-    $Jobdefs += New-Object PSCustomObject -Property @{JobName = $random; SID = $SID}
+    $Jobdefs += New-Object PSCustomObject -Property @{JobName = $random; SID = $SID; State = "Running"}
 }
+
+Write-Host "Waiting for all jobs to complete"
 
 $Completed = [System.Collections.ArrayList]::new()
 
 while($Completed.Count -ne ($Jobdefs | Measure-Object).Count)
 {
-    Write-Host "Complete count is $($Completed.Count) and total count is $(($Jobdefs | Measure-Object).Count)"
     Start-Sleep -Seconds $MonitoringIntervalInSeconds
+
+    Write-Host "$($Completed.Count) Jobs completed out of $(($Jobdefs | Measure-Object).Count)"
 
     foreach($Jobdef in $Jobdefs)
     {
@@ -57,12 +60,23 @@ while($Completed.Count -ne ($Jobdefs | Measure-Object).Count)
         {
             $Out = Receive-Job -Name $($Jobdef.JobName) | ConvertFrom-Json
 
-            Write-Host "The registration for $($Jobdef.SID) completed with provisioning state $($Out.properties.ProvisioningState)"
+            Write-Host "The registration for $($Jobdef.SID) completed with provisioning state as $($Out.properties.ProvisioningState)"
 
-            $Completed.Add($Jobdef.JobName)
+            $null = $Completed.Add($Jobdef.JobName)
+            
+            $filerec = $file | Where-Object {$_.SID -eq $Jobdef.SID}
+
+            $filerec.State = $Out.properties.ProvisioningState
+            
+            $Jobrec = $Jobdefs | Where-Object {$_.SID -eq $Jobdef.SID}
+            
+            $Jobrec.State = $Job.State
 
         }
-
-        Write-Host "Registration completed for $($Completed.Count) SIDs"
     }
 }
+
+$Jobdefs | Format-Table -AutoSize
+
+# Replace <filePath> with the path of the output CSV file path 
+$file | Export-Csv "C:\Users\jasksingh\Downloads\RegistrationData.csv" -Force -NoTypeInformation
